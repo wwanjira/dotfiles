44b1f8b2da5f171ead2d39bd260d7f854d1957c7
Wilber Wanjira <wwanjira@amazon.com>
2020-11-29 17:18:33 +1100

    fix untracked files + some changes

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,3 +1,9 @@
+# Easier navigation: .., ...
+alias ..="cd .."
+alias ...="cd ../.."
+
+# Shortcuts
 alias reload!='. ~/.zshrc'
+alias path='echo -e ${PATH//:/\\n}'
 
 alias cls='clear' # Good 'ol Clear Screen command
